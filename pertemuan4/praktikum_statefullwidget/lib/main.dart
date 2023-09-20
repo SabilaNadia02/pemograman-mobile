d1a3328b53e3790a42258fddcc3eaa0ec98ef59c
SabilaNadia02 <nadiasabilala@gmail.com>
2023-09-20 18:32:25 +0700

    task completed

--- pertemuan4/praktikum_statefullwidget/lib/main.dart
+++ pertemuan4/praktikum_statefullwidget/lib/main.dart
@@ -15,45 +15,71 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: false,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Konversi Suhu'),
+      appBar: AppBar(
+        title: const Text('Konversi Suhu'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(3),
+        child: const Center(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TemperatureConverter(),
+          ),
         ),
-        body: Container(
-            margin: EdgeInsets.all(3),
-            child: Center(
-                child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  MyForm(),
-                  MyDropDownButton(),
-                ],
-              ),
-            ))));
+      ),
+    );
   }
 }
 
-//Drop Down Button
-class MyDropDownButton extends StatefulWidget {
+// Temperatur Converter
+class TemperatureConverter extends StatefulWidget {
+  const TemperatureConverter({super.key});
+
   @override
-  _MyDropDownButtonState createState() => _MyDropDownButtonState();
+  _TemperatureConverterState createState() => _TemperatureConverterState();
 }
 
-class _MyDropDownButtonState extends State<MyDropDownButton> {
-  String _selectedItem = 'Kelvin';
+class _TemperatureConverterState extends State<TemperatureConverter> {
+  final TextEditingController _inputController = TextEditingController();
+  String _selectedOutputUnit = 'Fahrenheit';
+  final list = [];
 
-  void _onDropDownChanged(selectedItem) {
+  final Map<String, double> _unitValues = {
+    'Reamur': 0.0,
+    'Fahrenheit': 0.0,
+    'Kelvin': 0.0,
+  };
+
+  void _convert() {
+    double inputValue = double.tryParse(_inputController.text) ?? 0.0;
+    double outputValue;
+
+    if (_selectedOutputUnit == 'Fahrenheit') {
+      outputValue = (inputValue * 9 / 5) + 32;
+    } else if (_selectedOutputUnit == 'Kelvin') {
+      outputValue = inputValue + 273.15;
+    } else if (_selectedOutputUnit == 'Reamur') {
+      outputValue = inputValue * 4 / 5;
+    } else {
+      outputValue = 0;
+    }
+
+    // list.add(outputValue + " " + _selectedOutputUnit);
+    list.add('$outputValue $_selectedOutputUnit');
+
     setState(() {
-      _selectedItem = selectedItem;
+      _unitValues[_selectedOutputUnit] = outputValue;
     });
   }
 
@@ -61,41 +87,68 @@ class _MyDropDownButtonState extends State<MyDropDownButton> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        DropdownButton<String>(
-          value: _selectedItem,
-          onChanged: _onDropDownChanged,
-          items: <String>['Kelvin', 'Reamur'].map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-        ),
-      ],
-    );
-  }
-}
-
-// Form Suhu Input
-class MyForm extends StatefulWidget {
-  @override
-  _MyFormState createState() => _MyFormState();
-}
-
-class _MyFormState extends State<MyForm> {
-  final TextEditingController _controller = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
         TextFormField(
-          controller: _controller,
-          decoration: InputDecoration(
-            labelText: 'Masukkan Suhu',
+          controller: _inputController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            labelText: 'Masukkan suhu',
           ),
         ),
-        SizedBox(height: 20.0),
+        const SizedBox(height: 10.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            DropdownButton<String>(
+              value: _selectedOutputUnit,
+              onChanged: (newValue) {
+                setState(() {
+                  _selectedOutputUnit = newValue!;
+                });
+              },
+              items: _unitValues.keys.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+        const SizedBox(height: 18.0),
+        const Text(
+          'Hasil',
+          style: TextStyle(fontSize: 18.0),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 2.0),
+        Text(
+          '${_unitValues[_selectedOutputUnit]?.toStringAsFixed(2)}',
+          style: const TextStyle(fontSize: 20.0),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 18.0),
+        ElevatedButton(
+          onPressed: _convert,
+          style: ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 45)),
+          child: const Text(
+            'Konversi',
+            style: TextStyle(fontSize: 15.0),
+          ),
+        ),
+        const SizedBox(height: 20.0),
+        const Text('Riwayat Konversi', style: TextStyle(fontSize: 18.0)),
+        const SizedBox(height: 10.0),
+        Expanded(
+          child: ListView.builder(
+            itemCount: list.length,
+            reverse: true   ,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(list[index]),
+              );
+            },
+          ),
+        ),
       ],
     );
   }
